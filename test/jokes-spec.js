6fad14dc141a0d29eb6bdf2dc654e43cc2779f08
slk2370 <101383020+slk2370@users.noreply.github.com>
2022-08-13 20:56:01 -0600

    Update jokes-spec.js

--- test/jokes-spec.js
+++ test/jokes-spec.js
@@ -6,19 +6,12 @@ describe("Random Jokes", async function () {
   it("should get random joke", async function () {
     const joke1 = await jokesApi.getRandomJoke();
     const joke2 = await jokesApi.getRandomJoke();
-
-    // Verify status code
     expect(joke1.status).to.equal(200);
     assert.equal(joke1.status, 200);
     should.equal(joke1.status, 200);
-    //expect(joke2.status).to.equal(200);
-
-    // Verify user gets different jokes upon every request
     expect(joke1.data.value).not.to.equal(joke2.data.value);
     assert.notEqual(joke1.data.value, joke2.data.value);
     should.not.equal(joke1.data.value, joke2.data.value);
-
-    // Verify URL contains expected base of the url
     expect(joke1.data.url).to.include("https://api.chucknorris.io/jokes");
     assert.include(joke1.data.url, "https://api.chucknorris.io/jokes");
     should.include(joke1.data.url, "https://api.chucknorris.io/jokes");
